ba3ef1113b570f3c59412c2d8609d18780d6dc26
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-12 23:20:36 +0530

    initial commit

---
+++ Stacks/Questions/PushAtBottom.java
@@ -0,0 +1,28 @@
+package Stacks;
+
+import java.util.*;
+public class PushAtBottom {
+    public static void pushAtBottom(int data , Stack<Integer> s) {
+       if (s.isEmpty()) {
+           s.push(data);
+           return;
+       }
+
+       int temp = s.peek();
+       pushAtBottom(data, s);
+       s.push(temp);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtBottom(4, s);
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
---
+++ Stacks/Questions/Reverse.java
@@ -0,0 +1,2 @@
+package Stacks.Questions;public class Reverse {
+}
