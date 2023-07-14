673fdb12bf815052172d004ff2990b5ef5d81eda
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-14 10:18:39 +0530

    initial commit

---
+++ Queues/QueuesLL.java
@@ -0,0 +1,26 @@
+package Queues;
+
+public class QueueLL {
+    public static void main(String[] args) {
+
+    }
+
+
+    static Node head;
+    static Node tail;
+
+    static class Node {
+        int data;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+    public static void add(int data) {
+        if (head == null) {
+            
+        }
+    }
+
+}
