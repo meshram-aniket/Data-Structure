a4c281e55c7062c05703d19afbd76b356a7a4134
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-14 10:26:08 +0530

    initial commit

--- Queues/QueuesA.java
+++ Queues/QueuesA.java
@@ -1,4 +1,66 @@
 package Queues;
 
 public class QueuesA {
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        while(!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+
+
+    static class Queue {
+        static int[] array;
+        static int tail = -1;
+        static int size;
+        static int head = 0;
+        Queue(int num) {
+            array = new int[num];
+            this.size = num;
+        }
+
+
+        public static void add(int data) {
+           if (tail == size - 1) {
+               System.out.println("array is full");
+               return;
+           }
+           tail++;
+           array[tail] = data;
+
+        }
+
+
+        public static int remove() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int front = array[0];
+            for (int i = 0; i < tail; i++) {
+                array[i] = array[i + 1];
+            }
+            tail--;
+            return front;
+        }
+
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return array[0];
+        }
+
+        public static boolean isEmpty() {
+            return tail == -1;
+        }
+    }
+
 }
--- Queues/QueuesLL.java
+++ Queues/QueuesLL.java
@@ -1,26 +1,73 @@
 package Queues;
 
-public class QueueLL {
+import LinkList.LL;
+
+public class QueuesLL {
     public static void main(String[] args) {
-
+        QueuesLL l = new QueuesLL();
+        l.insertLast(5);
+        l.insertLast(6);
+        l.insertLast(8);
+        l.display();
     }
 
 
-    static Node head;
-    static Node tail;
+     private Node head;
+     private Node tail;
 
-    static class Node {
-        int data;
-        Node(int data) {
-            this.data = data;
+     private int size;
+     public QueuesLL() {
+         this.size = 0;
+     }
+
+     private class Node {
+         int value;
+         Node next;
+
+         Node(int data) {
+             this.value = data;
+         }
+     }
+
+    public void insertLast (int val) {
+        if (tail == null) {
+            tail = head;
         }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
     }
 
 
-    public static void add(int data) {
+    public int remove() {
+
         if (head == null) {
-            
+            return -1;
         }
+        int ret = head.value;
+        head = head.next;
+        size--;
+        return ret;
+    }
+
+
+    public int peek() {
+        if (head == null) {
+            return -1;
+        }
+
+        return head.value;
+    }
+
+
+    public void display() {
+        Node temp = head;
+        for (int i = 0; i < size; i++) {
+            System.out.print(temp.value+" -> ");
+            temp = temp.next;
+        }
+        System.out.println("end");
     }
 
 }
