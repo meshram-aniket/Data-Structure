29a10cb4b7384dff7fe7fe3d476b78b7827fa142
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-17 00:50:09 +0530

    initial commit

---
+++ Trees/BT.java
@@ -0,0 +1,22 @@
+package Trees;
+
+public class BT {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node (int val) {
+            this.val = val;
+        }
+
+        Node root;
+        public void populate (Node ) {
+
+        }
+    }
+}
