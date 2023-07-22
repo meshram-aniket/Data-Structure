d0cd2501aa0e580d4d567e523d70f4e1925b5cf1
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-23 00:20:33 +0530

    initial commit

--- HashMap/UnionOfArray.java
+++ HashMap/UnionOfArray.java
@@ -2,28 +2,28 @@ package HashMap;
 
 import java.util.HashSet;
 
-//public class UnionOfArray {
-//    public static void main(String[] args) {
-//        int[] arr1 =  {7, 3, 9};
-//        int[] arr2 = {6, 3, 9, 2, 9, 4};
-//        UnionOfArray u = new UnionOfArray();
-//        System.out.println( u.merge(arr1, arr2));
-//    }
-//
-//    public int merge(int[] arr1, int[] arr2) {
-//        HashSet<Integer> set = new HashSet<>();
-//
-//        for (int i = 0; i < arr1.length; i++) {
-//            set.add(arr1[i]);
-//        }
-//        for (int i = 0; i < arr2.length; i++) {
-//            set.add(arr2[i]);
-//        }
-//
-//        System.out.println(set);
-//
-//        System.out.print("the size is : " );
-//        return set.size();
-//    }
-//
-//}
+public class UnionOfArray {
+    public static void main(String[] args) {
+        int[] arr1 =  {7, 3, 9};
+        int[] arr2 = {6, 3, 9, 2, 9, 4};
+        UnionOfArray u = new UnionOfArray();
+        System.out.println( u.merge(arr1, arr2));
+    }
+
+    public int merge(int[] arr1, int[] arr2) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+
+        System.out.println(set);
+
+        System.out.print("the size is : " );
+        return set.size();
+    }
+
+}
