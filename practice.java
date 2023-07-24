54c70121cff3ae292150bb357b97a8785f519f11
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-25 00:14:00 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -1,20 +1,24 @@
-import LinkList.Question.Palindrome;
-import Trees.BST.deleteNode;
-
-import java.util.*;
 public class practice {
 
     public static void main(String[] args) {
         practice p = new practice();
-//        p.fibo
+        int num = 5;
+        int a = 0;
+        int b = 1;
+        System.out.print("0 1 ");
+        fib(num, a, b);
        
 
     }
 
-    public static void fibo(int start, int end) {
-        int sum = start + end;
-        System.out.println(sum);
-        fibo(end, sum);
+    public static void fib(int num, int start, int end) {
+        if (num == 0) {
+            return;
+        }
+            int sum = start + end;
+            System.out.print(sum + " ");
+            fib(num - 1, end, sum);
+
     }
 
 
