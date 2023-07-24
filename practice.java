c6deb1631e27216464d1a0e6ed143a3da16948ef
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-07-24 23:54:44 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -5,39 +5,21 @@ import java.util.*;
 public class practice {
 
     public static void main(String[] args) {
-        int[] arr = {10, 2, -2, -20, 10};
-        int k = -10;
-        System.out.println(union(arr, k));
-    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int num = sc.nextInt();
 
-
-    public static int union (int[] arr, int k) {
-        HashMap<Integer, Integer> set = new HashMap<>();
-        set.put(0, 1);
-        int sum = 0;
-        int freq = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-
-            if (set.containsKey(sum)) {
-                set.put(sum, set.get(sum) + 1);
-            }
-
-            else {
-                set.put(sum, 1);
-            }
-
-
-            if (set.containsKey(sum - k)) {
-                freq += set.get(sum - k);
-            }
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
         }
-        System.out.println(set);
-        return freq;
+        System.out.println(fact);
+       
 
     }
 
-//    public static void count(HashMap<Integer, Integer> map, int n) {
-//
-//    }
+    
+
+
+
 }
