9c6a7dfb3609f13b24a3613cc69bc2236b09c183
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-13 23:54:33 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -1,8 +1,14 @@
 public class Hello {
     public  static void main(String[] args) {
         int a = 10;
-        int b = 19;
+        int b = 1;
         System.out.println("hello world");
         System.out.println(a + b);
+        if (b >= 9) {
+            System.out.println("greateer");
+        }
+        else {
+            System.out.println("lesser");
+        }
     }
 }
