9374d197d2ce12be14cf3be5bbab7e2820bb08fe
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-13 23:51:57 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public  static void main(String[] args) {
+        int a = 10;
+        int b = 19;
         System.out.println("hello world");
+        System.out.println(a + b);
     }
 }
