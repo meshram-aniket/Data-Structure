66ee44cda34d9a01cefac5557fc4b5a0f32584cc
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-14 23:58:39 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class Hello {
         int a = 18;
         String s = (a >= 18) ? "greater" : "smaller";
         System.out.println(s);
+        System.out.println("aniket meshram");
+        System.out.println("jalaram nagar");
+        System.out.println("xavier institute of engineering");
 
     }
 }
