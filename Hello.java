89679d0a5cb9d2bcadbed70ebb941b1ebf33bf28
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-18 00:01:40 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -5,5 +5,36 @@ public class Hello {
         System.out.println("hello world");
         System.out.println("my name is aniket");
     }
+
+    public static int LargestArea(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        stack.push(0);
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] < height[stack.peek()]) {
+                max = getMax(stack, max, i, height);
+            }
+            stack.push(i);
+        }
+
+        int i = height.length;
+        while (!stack.isEmpty()) {
+            max = getMax(stack, max, i, height);
+        }
+        return max;
+    }
+
+    public static int getMax(Stack<Integer> stack, int max, int i, int[] height) {
+        int area;
+        int popped = stack.pop();
+
+        if (stack.isEmpty()) {
+            area = height[popped] * i;
+        }
+        else{
+            area = height[popped] * (i - 1 - stack.peek());
+        }
+        return Math.max(area, max);
+    }
 }
 
