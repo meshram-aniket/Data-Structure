3114fa8c93164ed51df732dbebce6940ade7ab5b
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-23 23:50:38 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -2,78 +2,11 @@ import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        populate(sc);
-        display();
-    }
-
-
-    public static class Node{
-        int val;
-        Node left;
-        Node right;
-
-        Node (int val) {
-            this.val = val;
-        }
-    }
-
-    private static Node root;
-    public static void populate(Scanner sc) {
-        System.out.println("enter the root of the tree: ");
-        int val = sc.nextInt();
-        root = new Node(val);
-        populate(root, sc);
-    }
-
-
-    private static void populate(Node node, Scanner sc) {
-        System.out.println("do you want to enter the left of " + node.val + "?");
-        boolean left = sc.nextBoolean();
-
-        if (left) {
-            System.out.println("enter the the left of the "+ node.val);
-            int val = sc.nextInt();
-            node.left = new Node(val);
-            populate(node.left, sc);
-        }
-
-
-        System.out.println("do you want to enter the right of " + node.val + "?");
-        boolean right = sc.nextBoolean();
-
-        if (right) {
-            System.out.println("enter the the right of the "+ node.val);
-            int val = sc.nextInt();
-            node.right = new Node(val);
-            populate(node.right, sc);
-        }
-    }
-
-
-    public static void display() {
-        display(root, 0);
-    }
-
-
-    public static void display(Node node, int level) {
-        if (node == null) {
-            return;
-        }
-
-        display(node.right, level + 1);
-        if (level != 0) {
-            for (int i = 0; i < level - 1; i++) {
-                System.out.print("|\t\t\t");
-            }
-            System.out.println("|------>" + node.val);
-        }
-
-        else {
-            System.out.println(node.val);;
-        }
-
-        display(node.left, level + 1);
+        System.out.println("Aniket Pramod Meshram");
+        System.out.println("Jalaram Nagar Amravati");
+        System.out.println("Xavier Institute of Engineering, Mahim");
+        System.out.println("Veermata Jijabai Technological Institute");
+        System.out.println("St Francis High School");
     }
 }
 
