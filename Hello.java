88a0c6c9680e93de14e210e925d6ef6432724006
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-23 23:58:22 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -2,21 +2,15 @@ import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 8, 0};
-        System.out.println(getMax(arr));
-
-    }
-
-
-    public static int getMax(int[] arr) {
-        int max = 0;
+        int[] arr = new int[6];
+        Scanner sc = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < max) {
-                max = arr[i];
-            }
+            arr[i] = sc.nextInt();
         }
-//        System.out.println(max);
-        return max;
+        System.out.println(Arrays.toString(arr));
+
     }
+
+
 }
 
