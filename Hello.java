da41341cf89c62f865d21b929cd1e9e72f2b83d0
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-08-27 01:49:56 +0530

    initial commit

--- Hello.java
+++ Hello.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
-    
+
         System.out.print( "0 1 ");
         fibonachi(0,1,0,5);
+        reverse(0);
 
     }
 
@@ -17,6 +18,16 @@ public class Hello {
         sum = a + b;
         System.out.print(sum + " ");
         fibonachi(b, sum, 0, n - 1);
+    }
+
+    public static void reverse(int num) {
+        if (num == 5) {
+            System.out.println(num);
+            return;
+        }
+
+        reverse(num + 1);
+        System.out.println(num);
     }
 }
 
