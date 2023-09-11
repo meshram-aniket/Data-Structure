89346c5cc737534f3ebc34f820d12a8f95da7519
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-09-12 00:03:49 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -4,26 +4,15 @@ import java.util.LinkedList;
 public class practice {
     public static void main(String[] args) {
         int[] arr = {1,4,6,8,12,43,50,60,100,120,130};
-        System.out.println(BinarySearch(arr, 5));
+        System.out.println(linearSearch(arr, 50));
     }
 
 
-    public static int BinarySearch(int[] arr, int target) {
-        int start = 0;
-        int end = arr.length - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (target < arr[mid]) {
-                end = mid - 1;
-            }
-
-            else {
-                if (target > arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    return mid;
-                }
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                return i;
             }
         }
         return -1;
