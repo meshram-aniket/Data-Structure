5e5d3f95f3b7af6d4c646d0b6565c1183556ad4a
Aniket Meshram <aniketmeshramofficial@gmail.com>
2023-09-13 23:55:42 +0530

    initial commit

--- practice.java
+++ practice.java
@@ -4,19 +4,27 @@ import java.util.LinkedList;
 public class practice {
     public static void main(String[] args) {
         int number = 1234;
-        System.out.println(reverse(number));;
+        System.out.println(reverse(number));
+        System.out.println(odd(7));
     }
 
     static int sum = 0;
     public static int reverse(int num) {
         if (num == 0) {
-//            System.out.println(sum);
             return 0;
         }
         int remainder = num % 10;
         sum = (sum * 10) + remainder;
         reverse(num / 10);
         return sum;
+    }
+
+
+    public static boolean odd(int num) {
+        if (num % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 }
 
